f5479e59439870e8d1735b8d62ef8e4067716b89
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-08 15:24:09 +0200

    settings page refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Galleries from "./components/Galleries";
 import PageFooter from "./components/PageFooter";
+import Settings from "./components/Settings";
 import TopBar from "./components/TopBar";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Galleries />
+      {/* <Galleries /> */}
+      <Settings />
       <PageFooter />
     </>
   );
--- src/components/Galleries.jsx
+++ src/components/Galleries.jsx
@@ -1,8 +1,28 @@
+import { Container, Dropdown } from "react-bootstrap";
 import Gallery from "./Gallery";
 
 const Galleries = () => {
   return (
     <>
+      <Container fluid className="ps-md-5 d-flex mb-4">
+        <h3 className="fw-bold me-3">TV Shows</h3>
+        <Dropdown>
+          <Dropdown.Toggle variant="border-secondary text-white btn-outline-secondary ">Genres</Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="index.html">Western</Dropdown.Item>
+            <Dropdown.Item href="index.html">Brutti</Dropdown.Item>
+            <Dropdown.Item href="index.html">Porn</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <div className="d-flex ms-auto align-items-center">
+          <a className="text-secondary view-box" href="index.html">
+            <i className="bi bi-text-left border px-3 py-1"></i>
+          </a>
+          <a className="text-secondary view-box" href="index.html">
+            <i className="bi bi-grid border border px-3 py-1"></i>
+          </a>
+        </div>
+      </Container>
       <Gallery show="Star Trek" />
       <Gallery show="Star Wars" />
       <Gallery show="Fast and Furious" />
